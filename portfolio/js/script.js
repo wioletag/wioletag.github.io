39e169fd648b9718273ccf184a6ea68c4dfb90fc
Wioleta <wioletag@users.noreply.github.com>
2017-08-15 21:47:29 -0500

    Add files via upload

---
+++ portfolio/js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+	"use strict";
+
+	// jQuery for smooth scrolling
+    $('a.scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+});
